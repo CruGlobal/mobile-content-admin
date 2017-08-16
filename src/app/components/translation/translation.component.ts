a46082e9355ba7652c1669c5d8a99bf016f6ef40
dsgoers <daniel.goers@cru.org>
2017-08-16 17:31:26 -0400

    Refactor

--- src/app/components/translation/translation.component.ts
+++ src/app/components/translation/translation.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Translation} from '../../models/translation';
 import {DraftService} from '../../service/draft.service';
-import {Page} from '../../models/page';
 import {CustomPage} from '../../models/custom-page';
+import {AbstractPage} from '../../models/abstract-page';
 
 @Component({
   selector: 'admin-translation',
@@ -13,19 +13,11 @@ export class TranslationComponent {
 
   constructor(private draftService: DraftService) {}
 
-  getPages(): Page[] {
-    const pages = [];
-
-    for (const page of this.translation.resource.pages) {
+  getPages(): AbstractPage[] {
+    return this.translation.resource.pages.map(page => {
       const customPage: CustomPage = this.translation.language['custom-pages'].find(c => c.page.id === page.id);
-      if (customPage != null) {
-        pages.push(customPage);
-      } else {
-        pages.push(page);
-      }
-    }
-
-    return pages;
+      return customPage == null ? page : customPage;
+    });
   }
 
   createDraft(): void {
