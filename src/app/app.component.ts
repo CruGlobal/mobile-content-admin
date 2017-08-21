5d465cf6ed32ff93b520b365c218722274985998
dsgoers <daniel.goers@cru.org>
2017-08-21 16:48:37 -0400

    Move navbar to separate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'admin-app',
-  template: `
-   <h1>{{title}}</h1>
-   <nav>
-     <a routerLink="/dashboard">Dashboard</a>
-     <a routerLink="/resources">Resources</a>
-     <a routerLink="/attachments">Attachments</a>
-     <a routerLink="/languages">Languages</a>
-   </nav>
-   <router-outlet></router-outlet>
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'Mobile Content Admin';
