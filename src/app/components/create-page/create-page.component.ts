edbd0be974e9c3d1ebcbeeeefb60b40f0364a8dd
dsgoers <daniel.goers@cru.org>
2017-08-24 17:19:11 -0400

    Save page when button is clicked

--- src/app/components/create-page/create-page.component.ts
+++ src/app/components/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Page} from '../../models/page';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {PageService} from '../../service/page.service';
 
 @Component({
   selector: 'admin-create-page',
@@ -9,10 +10,12 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 export class CreatePageComponent {
   @Input() page: Page = new Page();
 
-  constructor(private activeModal: NgbActiveModal) {}
+  constructor(private pageService: PageService, private activeModal: NgbActiveModal) {}
 
   savePage(): void {
-    this.activeModal.close();
+    this.pageService.create(this.page)
+      .then(() => this.activeModal.close())
+      .catch();
   }
 
   dismissModal(): void {
