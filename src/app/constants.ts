70044e779346457afa65eee3d20902aa355510b6
dsgoers <daniel.goers@cru.org>
2017-08-24 09:41:57 -0400

    Add staging environment config

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,8 @@
 import {Headers, RequestOptionsArgs} from '@angular/http';
+import { environment } from '../environments/environment';
 
 export class Constants {
-  public static readonly BASE_URL = 'http://localhost:3000/';
+  public static readonly BASE_URL = environment.base_url;
   public static readonly OPTIONS: RequestOptionsArgs = {
     headers : new Headers({'Content-Type': 'application/vnd.api+json'})
   };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  base_url: 'https://mobile-content-api-stage.cru.org/'
+};
