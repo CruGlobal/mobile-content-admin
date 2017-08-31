806479d8e91258b2a9c93dff704a60873d6971af
dsgoers <daniel.goers@cru.org>
2017-08-31 15:34:27 -0400

    Add some tests for PageComponent

---
+++ src/app/components/page/page.component.spec.ts
@@ -0,0 +1,60 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {XmlEditorComponent} from '../xml-editor/xml-editor.component';
+import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AceEditorDirective} from 'ng2-ace-editor';
+import {CustomPageService} from '../../service/custom-page.service';
+import {PageService} from '../../service/page.service';
+import {Page} from '../../models/page';
+import {PageComponent} from './page.component';
+import {Translation} from '../../models/translation';
+import {Language} from '../../models/language';
+
+describe('PageComponent', () => {
+  let comp:    PageComponent;
+  let fixture: ComponentFixture<PageComponent>;
+  let xmlEditor: DebugElement;
+
+  const customPageServiceStub = {
+    upsert() {}
+  };
+  const pageServiceStub = {
+    update() {}
+  };
+
+  beforeEach(() => {
+    spyOn(customPageServiceStub, 'upsert');
+    spyOn(pageServiceStub, 'update');
+
+    TestBed.configureTestingModule({
+      declarations: [ PageComponent, XmlEditorComponent, AceEditorDirective ],
+      imports: [ NgbModule.forRoot() ],
+      providers: [
+        {provide: PageService, useValue: pageServiceStub},
+        {provide: CustomPageService, useValue: customPageServiceStub},
+        {provide: NgbActiveModal}
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PageComponent);
+    comp = fixture.componentInstance;
+    comp.translation = new Translation();
+    comp.translation.language = new Language();
+    comp.page = new Page();
+
+    xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+  });
+
+  it('creates new CustomPage when XmlEditorComponent saves for one', () => {
+    xmlEditor.triggerEventHandler('onSaveForOne', 'emit');
+
+    expect(customPageServiceStub.upsert).toHaveBeenCalled();
+  });
+
+  it('updates Page when XmlEditorComponent saves for all', () => {
+    xmlEditor.triggerEventHandler('onSaveForAll', 'emit');
+
+    expect(pageServiceStub.update).toHaveBeenCalled();
+  });
+
+});
