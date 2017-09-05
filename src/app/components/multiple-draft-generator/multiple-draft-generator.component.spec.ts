997dfeaee0ae34b9f1f2d6bd61b6513fbce56536
dsgoers <daniel.goers@cru.org>
2017-09-05 16:31:45 -0400

    Test that only languages without drafts are shown in draft generator

--- src/app/components/multiple-draft-generator/multiple-draft-generator.component.spec.ts
+++ src/app/components/multiple-draft-generator/multiple-draft-generator.component.spec.ts
@@ -3,10 +3,22 @@ import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {MultipleDraftGeneratorComponent} from './multiple-draft-generator.component';
 import {FormsModule} from '@angular/forms';
 import {DraftService} from '../../service/draft.service';
+import {Resource} from '../../models/resource';
+import {Translation} from '../../models/translation';
+import {By} from '@angular/platform-browser';
+import {NgbButtonLabel} from '@ng-bootstrap/ng-bootstrap/buttons/label';
+import {Language} from '../../models/language';
 
 describe('MultipleDraftGeneratorComponent', () => {
   let comp: MultipleDraftGeneratorComponent;
   let fixture: ComponentFixture<MultipleDraftGeneratorComponent>;
+
+  const buildTranslation = (is_published) => {
+    const t = new Translation();
+    t.language = new Language();
+    t.is_published = is_published;
+    return t;
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -20,9 +32,15 @@ describe('MultipleDraftGeneratorComponent', () => {
 
     fixture = TestBed.createComponent(MultipleDraftGeneratorComponent);
     comp = fixture.componentInstance;
+
+    const r = new Resource();
+    r['latest-drafts-translations'] = [ buildTranslation(true), buildTranslation(false), buildTranslation(true) ];
+    comp.resource = r;
+
+    fixture.detectChanges();
   });
 
-  it('only shows published translations', () => {
-
+  it('only shows languages without drafts', () => {
+    expect(fixture.debugElement.queryAll(By.directive(NgbButtonLabel)).length).toBe(2);
   });
 });
