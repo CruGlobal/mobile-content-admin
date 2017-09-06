aaae994e2f2f82e94d6d5fee9b4bb875196484b0
dsgoers <daniel.goers@cru.org>
2017-09-06 18:41:08 -0400

    Add tests

--- src/app/service/resource/resource.service.spec.ts
+++ src/app/service/resource/resource.service.spec.ts
@@ -1,23 +1,43 @@
 import 'rxjs/add/operator/toPromise';
 import {ResourceService} from './resource.service';
-import {Http} from '@angular/http';
+import {Http, RequestOptionsArgs} from '@angular/http';
 import {AuthService} from '../auth.service';
+import {Resource} from '../../models/resource';
+import {Observable} from 'rxjs/Observable';
+import anything = jasmine.anything;
+
+const headers: RequestOptionsArgs = {};
 
 class MockHttp extends Http {
 }
 
 class MockAuthService extends AuthService {
+  getAuthorizationAndOptions() {
+    return headers;
+  }
 }
 
 describe ('AuthGuardService', () => {
   const mockHttp = new MockHttp(null, null);
   const mockAuthService = new MockAuthService(null, null);
-
   const service = new ResourceService(mockHttp, mockAuthService);
 
+  const resource = new Resource();
+
+  beforeEach(() => {
+    spyOn(mockHttp, 'post').and.returnValue(Observable.create(resource));
+    spyOn(mockHttp, 'put').and.returnValue(Observable.create());
+  });
+
   it('creating uses authorization code', () => {
+    service.create(resource);
+
+    expect(mockHttp.post).toHaveBeenCalledWith(anything(), anything(), headers);
   });
 
   it('updating uses authorization code', () => {
+    service.update(resource);
+
+    expect(mockHttp.put).toHaveBeenCalledWith(anything(), anything(), headers);
   });
 });
