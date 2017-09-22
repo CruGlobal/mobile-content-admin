016c0581a480d761d47ee783473c2b3e44620ffa
dsgoers <daniel.goers@cru.org>
2017-09-22 11:32:37 -0400

    Fix tests

--- src/app/components/custom-page/custom-page.component.spec.ts
+++ src/app/components/custom-page/custom-page.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CustomPageComponent} from './custom-page.component';
 import {DebugElement} from '@angular/core';
 import {XmlEditorComponent} from '../xml-editor/xml-editor.component';
@@ -10,6 +10,7 @@ import {CustomPage} from '../../models/custom-page';
 import {Language} from '../../models/language';
 import {Page} from '../../models/page';
 import {DraftService} from '../../service/draft.service';
+import {Resource} from '../../models/resource';
 
 describe('CustomPageComponent', () => {
   let comp:    CustomPageComponent;
@@ -22,8 +23,11 @@ describe('CustomPageComponent', () => {
   const pageServiceStub = {
     update() {}
   };
+  const draftServiceStub = {
+    getPage() { return Promise.resolve('xml response'); }
+  };
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     spyOn(customPageServiceStub, 'upsert').and.returnValue(Promise.resolve());
     spyOn(pageServiceStub, 'update').and.returnValue(Promise.resolve());
 
@@ -33,30 +37,47 @@ describe('CustomPageComponent', () => {
       providers: [
         {provide: PageService, useValue: pageServiceStub},
         {provide: CustomPageService, useValue: customPageServiceStub},
-        {provide: DraftService},
+        {provide: DraftService, useValue: draftServiceStub},
         {provide: NgbActiveModal}
       ]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CustomPageComponent);
     comp = fixture.componentInstance;
     comp.customPage = new CustomPage();
     comp.customPage.language = new Language();
     comp.customPage.page = new Page();
-
-    xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+    comp.customPage.page.resource = new Resource();
   });
 
-  it('updates existing CustomPage when XmlEditorComponent saves for one', () => {
-    xmlEditor.triggerEventHandler('onSaveForOne', 'emit');
+  it('updates existing CustomPage when XmlEditorComponent saves for one', (done) => {
+    comp.ngOnInit();
 
-    expect(customPageServiceStub.upsert).toHaveBeenCalled();
+    setTimeout(() => {
+      fixture.detectChanges();
+      xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+
+      xmlEditor.triggerEventHandler('onSaveForOne', 'emit');
+
+      expect(customPageServiceStub.upsert).toHaveBeenCalled();
+      done();
+    });
   });
 
-  it('updates Page when XmlEditorComponent saves for all', () => {
-    xmlEditor.triggerEventHandler('onSaveForAll', 'emit');
+  it('updates Page when XmlEditorComponent saves for all', (done) => {
+    comp.ngOnInit();
 
-    expect(pageServiceStub.update).toHaveBeenCalled();
+    setTimeout(() => {
+      fixture.detectChanges();
+      xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+
+      xmlEditor.triggerEventHandler('onSaveForAll', 'emit');
+
+      expect(pageServiceStub.update).toHaveBeenCalled();
+      done();
+    });
   });
 
 });
--- src/app/components/page/page.component.spec.ts
+++ src/app/components/page/page.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {DebugElement} from '@angular/core';
 import {XmlEditorComponent} from '../xml-editor/xml-editor.component';
 import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -10,6 +10,7 @@ import {PageComponent} from './page.component';
 import {Translation} from '../../models/translation';
 import {Language} from '../../models/language';
 import {DraftService} from '../../service/draft.service';
+import {Resource} from '../../models/resource';
 
 describe('PageComponent', () => {
   let comp:    PageComponent;
@@ -22,8 +23,11 @@ describe('PageComponent', () => {
   const pageServiceStub = {
     update() {}
   };
+  const draftServiceStub = {
+    getPage() { return Promise.resolve('xml response'); }
+  };
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     spyOn(customPageServiceStub, 'upsert').and.returnValue(Promise.resolve());
     spyOn(pageServiceStub, 'update').and.returnValue(Promise.resolve());
 
@@ -33,30 +37,47 @@ describe('PageComponent', () => {
       providers: [
         {provide: PageService, useValue: pageServiceStub},
         {provide: CustomPageService, useValue: customPageServiceStub},
-        {provide: DraftService},
+        {provide: DraftService, useValue: draftServiceStub},
         {provide: NgbActiveModal}
       ]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PageComponent);
     comp = fixture.componentInstance;
     comp.translation = new Translation();
     comp.translation.language = new Language();
     comp.page = new Page();
-
-    xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+    comp.page.resource = new Resource();
   });
 
-  it('creates new CustomPage when XmlEditorComponent saves for one', () => {
-    xmlEditor.triggerEventHandler('onSaveForOne', 'emit');
+  it('creates new CustomPage when XmlEditorComponent saves for one', (done) => {
+    comp.ngOnInit();
 
-    expect(customPageServiceStub.upsert).toHaveBeenCalled();
+    setTimeout(() => {
+      fixture.detectChanges();
+      xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+
+      xmlEditor.triggerEventHandler('onSaveForOne', 'emit');
+
+      expect(customPageServiceStub.upsert).toHaveBeenCalled();
+      done();
+    });
   });
 
-  it('updates Page when XmlEditorComponent saves for all', () => {
-    xmlEditor.triggerEventHandler('onSaveForAll', 'emit');
+  it('updates Page when XmlEditorComponent saves for all', (done) => {
+    comp.ngOnInit();
 
-    expect(pageServiceStub.update).toHaveBeenCalled();
+    setTimeout(() => {
+      fixture.detectChanges();
+      xmlEditor = fixture.debugElement.query(de => de.name === 'admin-xml-editor');
+
+      xmlEditor.triggerEventHandler('onSaveForAll', 'emit');
+
+      expect(pageServiceStub.update).toHaveBeenCalled();
+      done();
+    });
   });
 
 });
