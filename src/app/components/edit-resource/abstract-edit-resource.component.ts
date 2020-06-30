f276892a0f16ea9bbb8051376aea13b59c6c4fbc
Daniel Frett <daniel.frett@ccci.org>
2020-06-30 19:30:18 -0400

    workaround this bug: https://github.com/ajaxorg/ace/issues/4042
    ng2-ace-editor bundles a version of the ace editor without the fix.
    ng2-ace-editor appears to be abandoned, there is a new ngx-ace-editor, but it is for Angular 9

--- src/app/components/edit-resource/abstract-edit-resource.component.ts
+++ src/app/components/edit-resource/abstract-edit-resource.component.ts
@@ -1,14 +1,17 @@
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {Input} from '@angular/core';
+import {Input, OnDestroy, ViewChild} from '@angular/core';
 import {Resource} from '../../models/resource';
 import {ResourceType} from '../../models/resource-type';
 import {System} from '../../models/system';
 import {SystemService} from '../../service/system.service';
 import {ResourceTypeService} from '../../service/resource-type.service';
+import {AceEditorDirective} from 'ng2-ace-editor';
 
-export abstract class AbstractEditResourceComponent {
+export abstract class AbstractEditResourceComponent implements OnDestroy {
   protected saving = false;
   protected errorMessage: string;
+
+  @ViewChild(AceEditorDirective) editor;
 
   @Input() resource: Resource = new Resource();
   resourceTypes: ResourceType[];
@@ -34,6 +37,12 @@ export abstract class AbstractEditResourceComponent {
         systemsCallback.call();
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    // HACK: workaround this bug: https://github.com/ajaxorg/ace/issues/4042
+    //       ng2-ace-editor bundles an older version of ace that doesn't have this fix
+    this.editor.editor.renderer.freeze();
   }
 
   closeEditModal() {
