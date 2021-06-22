23cdf0f165b7fd99e81c1b19ccdf7282c8309071
Erman Celikceviren <ermancel@hotmail.com>
2021-06-23 00:55:49 +0300

    GT-473: Fix auth fuard service test

--- src/app/service/auth-guard/auth-guard.service.ts
+++ src/app/service/auth-guard/auth-guard.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { OklaLoginErrorComponent } from '../../components/okla-login-error/okla-login-error.component';
 import { DraftService } from '../draft.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
   constructor(
     private draftService: DraftService,
+    private modalService: NgbModal,
     private router: Router,
   ) {}
 
@@ -18,6 +21,12 @@ export class AuthGuardService implements CanActivate {
       })
       .catch(() => {
         console.log('User is not authenticated.');
+        const errorMessage = 'Login required.';
+        const modalRef: NgbModalRef = this.modalService.open(
+          OklaLoginErrorComponent,
+          { size: 'lg' },
+        );
+        modalRef.componentInstance.errorMessage = errorMessage;
         this.router.navigate(['/', 'login', 'callback']);
         return false;
       });
