ee327117d0012b0dbb53ffcff57a43faa83cc283
Gyasi Story <gyasistory@gmail.com>
2022-02-11 16:16:20 -0500

    GT-1455 : Create Spotlight attribute (#525)
    * add Spotlight attribute
    
    * Update resource.service.ts
    
    * Update edit-resource.component.html
    
    * Fix Build error
    
    * Use appropriate label name and attribute name
    
    * More attr prefix added
    
    * Update src/app/components/edit-resource/edit-resource.component.html
    
    Co-authored-by: Daniel Frett <frett@users.noreply.github.com>
    
    * Update src/app/components/edit-resource/edit-resource.component.html
    
    Co-authored-by: Daniel Frett <frett@users.noreply.github.com>
    
    * Update src/app/models/resource.ts
    
    Co-authored-by: Daniel Frett <frett@users.noreply.github.com>
    
    * Update src/app/service/resource/resource.service.ts
    
    Co-authored-by: Daniel Frett <frett@users.noreply.github.com>
    
    * GT-1455 Align side by side
    
    * GT-1455 switch locations and using col-3
    
    * fix prettier issue
    
    Co-authored-by: Daniel Frett <frett@users.noreply.github.com>
    Co-authored-by: Daniel Frett <daniel.frett@ccci.org>

--- src/app/service/resource/resource.service.ts
+++ src/app/service/resource/resource.service.ts
@@ -80,6 +80,7 @@ export class ResourceService extends AbstractService {
           'attr-category': resource['attr-category'] || null,
           'attr-default-order': resource['attr-default-order'] || null,
           'attr-hidden': resource['attr-hidden'] || null,
+          'attr-spotlight': resource['attr-spotlight'],
         },
       },
     };
