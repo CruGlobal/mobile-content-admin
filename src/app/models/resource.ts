508bbca54e713ddf421bf515491e215a3f32fd83
Gyasi Story <gyasistory@gmail.com>
2022-03-17 08:51:56 -0400

    Gt 1455 resolve issue with spotlight (#539)
    * GT-1455 make Boolean nullable
    
    * GT-1455 add nullable to attribute

--- src/app/models/resource.ts
+++ src/app/models/resource.ts
@@ -34,7 +34,7 @@ export class Resource {
     | 'training';
   'attr-default-order'?: number;
   'attr-hidden'?: boolean;
-  'attr-spotlight': boolean;
+  'attr-spotlight'?: boolean;
 
   static getResourceTypeId(resource: Resource): number {
     return resource.resourceType ? resource.resourceType.id : null;
