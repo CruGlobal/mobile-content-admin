16c160540affe0c8bf6f13e9030db13f135ea33d
Daniel Bisgrove <daniel@bizz-websites.com>
2023-10-05 14:08:04 -0400

    Building a basic add ToolGroup modal and toolGroup Serice with Create ToolGroup, Create Country and Language rules.

---
+++ src/app/models/tool-group.ts
@@ -0,0 +1,12 @@
+export class ToolGroup {
+  id: number;
+  name: string;
+  suggestedWeight: string;
+  'rules-country': any;
+  'rules-language': any;
+  'rules-praxis': any;
+  tools: any;
+  _attributes: string[];
+  _relationships: string[];
+  _type: string;
+}
