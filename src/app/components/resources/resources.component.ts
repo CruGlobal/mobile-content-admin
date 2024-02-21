afdbf53ddaa88c417b9d620419068a4dad4d70f2
Caleb Alldrin <caleb.alldrin@cru.org>
2024-02-21 14:41:57 -0800

    Style filter sort bar and syntax fixes

--- src/app/components/resources/resources.component.ts
+++ src/app/components/resources/resources.component.ts
@@ -17,12 +17,12 @@ import { ResourceTypeService } from '../../../../src/app/service/resource-type.s
 })
 export class ResourcesComponent implements OnInit {
   resources: Resource[];
+  unfilteredResources: Resource[];
   languages: Language[];
   resourceTypes: ResourceType[];
   systems: System[];
 
   showInstructions = false;
-  showFilters = false;
   loadingResources = false;
   loadingLanguages = false;
   errorMessage: string;
@@ -44,7 +44,7 @@ export class ResourcesComponent implements OnInit {
     this.loadFilters();
   }
 
-  blankFilters(): Filters {
+  getBlankFilters(): Filters {
     return {
       type: [],
       system: [],
@@ -58,51 +58,54 @@ export class ResourcesComponent implements OnInit {
         'latest-drafts-translations,pages,custom-manifests,tips,attachments,variants',
       )
       .then((resources) => {
-        const localFilters = localStorage.getItem('filters');
-        this.sortOrder = localStorage.getItem('sortOrder');
-        this.filters = localFilters
-          ? JSON.parse(localFilters)
-          : this.blankFilters();
-        this.totalFilters = Object.keys(this.filters).reduce(
-          (acc, item) => acc + this.filters[item].length,
-          0,
-        );
-
-        this.resources = this.filters
-          ? resources.filter((r) => {
-              let visible = true;
-              if (this.filters['type'].length) {
-                if (!this.filters['type'].includes(r['resource-type'])) {
-                  visible = false;
-                }
-              }
-              if (this.filters['system'].length) {
-                if (!this.filters['system'].includes(r['system']['id'])) {
-                  visible = false;
-                }
-              }
-              if (this.filters['other'].length) {
-                if (
-                  !r['attr-hidden'] ||
-                  (r['attr-hidden'] &&
-                    !this.filters['other'].includes('hidden'))
-                ) {
-                  visible = false;
-                }
-              }
-              return visible;
-            })
-          : resources;
-        if (this.sortOrder === 'default') {
-          this.resources.sort(
-            (a, b) =>
-              (a['attr-default-order'] || 100) -
-              (b['attr-default-order'] || 100),
-          );
-        }
+        this.unfilteredResources = resources;
+        this.filterAndSort();
       })
       .catch(this.handleError.bind(this))
       .then(() => (this.loadingResources = false));
+  }
+  filterAndSort(): void {
+    const localFilters = localStorage.getItem('filters');
+    this.sortOrder = localStorage.getItem('sortOrder');
+    this.filters = localFilters
+      ? JSON.parse(localFilters)
+      : this.getBlankFilters();
+    this.totalFilters = Object.keys(this.filters).reduce(
+      (acc, item) => acc + this.filters[item].length,
+      0,
+    );
+
+    this.resources = this.filters
+      ? this.unfilteredResources.filter((resource) => {
+          if (this.filters['type'].length) {
+            if (!this.filters['type'].includes(resource['resource-type'])) {
+              return false;
+            }
+          }
+          if (this.filters['system'].length) {
+            if (!this.filters['system'].includes(resource['system']['id'])) {
+              return false;
+            }
+          }
+          if (this.filters['other'].length) {
+            if (
+              !resource['attr-hidden'] ||
+              (resource['attr-hidden'] &&
+                !this.filters['other'].includes('hidden'))
+            ) {
+              return false;
+            }
+          }
+          return true;
+        })
+      : this.unfilteredResources;
+
+    if (this.sortOrder === 'tool') {
+      this.resources.sort(
+        (a, b) =>
+          (a['attr-default-order'] || 100) - (b['attr-default-order'] || 100),
+      );
+    }
   }
   loadFilters(): void {
     this.resourceTypeService.getResourceTypes().then((types) => {
@@ -124,32 +127,31 @@ export class ResourcesComponent implements OnInit {
     category: string,
     optionId: string | number,
   ): void {
-    const updatedFilters =
-      this.filters[category] && this.filters[category].includes(optionId)
+    const updatedFilters = this.filters[category]
+      ? this.filters[category].includes(optionId)
         ? {
             ...this.filters,
             [category]: this.filters[category].filter((e) => e !== optionId),
           }
-        : this.filters[category]
-        ? {
+        : {
             ...this.filters,
             [category]: [...this.filters[category], optionId],
           }
-        : {
-            ...this.filters,
-            [category]: [optionId],
-          };
+      : {
+          ...this.filters,
+          [category]: [optionId],
+        };
     localStorage.setItem('filters', JSON.stringify(updatedFilters));
-    this.loadResources();
+    this.filterAndSort();
   };
 
   clearFilters = function (): void {
-    localStorage.setItem('filters', JSON.stringify(this.blankFilters()));
-    this.loadResources();
+    localStorage.setItem('filters', JSON.stringify(this.getBlankFilters()));
+    this.filterAndSort();
   };
   updateSort = function (order: string): void {
     localStorage.setItem('sortOrder', order);
-    this.loadResources();
+    this.filterAndSort();
   };
 
   trackByFunction(pIx: number, pItem: Resource) {
