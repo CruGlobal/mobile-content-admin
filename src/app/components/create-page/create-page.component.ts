b85877241e84e39e0c6f92d261d1c3b6a74e03ed
Daniel Bisgrove <daniel@bizz-websites.com>
2025-07-25 12:23:59 -0400

    Fixed components that use `ng2-ace-editor`, `this.editor` was undefined during testing, causing an error.

--- src/app/components/create-page/create-page.component.ts
+++ src/app/components/create-page/create-page.component.ts
@@ -38,7 +38,7 @@ export class CreatePageComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     // HACK: workaround this bug: https://github.com/ajaxorg/ace/issues/4042
-    //       ng2-ace-editor bundles an older version of ace that doesn't have this fix
-    this.editor.editor.renderer.freeze();
+    //       ng2-ace-editor uses brace@0.11.1 which bundles an older version of ace without the fix
+    this.editor?.editor?.renderer?.freeze();
   }
 }
