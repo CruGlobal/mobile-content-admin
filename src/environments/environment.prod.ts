15db757bd696d05d457a2e20a7ae53b38d0212af
Daniel Frett <daniel.frett@cru.org>
2025-09-30 09:58:21 -0600

    remove unused config parameter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ export const environment = {
   base_url: 'https://mobile-content-api.cru.org/',
   oidc_auth: {
     client_id: '0oa1ll7gg0vVRLctz0h8',
-    client_secret: '738c0f49-b689-437a-9920-4f4f1fbfb7bb',
     scope: 'openid profile email',
     response_type: 'code',
     issuer: 'https://signon.okta.com',
